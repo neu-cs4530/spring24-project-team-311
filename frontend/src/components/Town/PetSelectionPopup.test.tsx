6eb6f03e509e6caa0d9285a7da52fa61f454779f
helenm1 <95582910+helenm1@users.noreply.github.com>
2024-03-13 01:03:46 -0400

    adjust image alts for getByText calls

--- frontend/src/components/Town/PetSelectionPopup.test.tsx
+++ frontend/src/components/Town/PetSelectionPopup.test.tsx
@@ -11,18 +11,29 @@ jest.mock('nanoid', () => ({
 jest.mock('./images/dog.png', () => 'dog-image-placeholder');
 
 describe('PetSelectionPopup', () => {
-  it('should display error message when user clicks "Done" without selecting a pet', () => {
+  beforeEach(() => {
     const onClose = jest.fn();
     render(<PetSelectionPopup isOpen={true} onClose={onClose} />);
+  });
+  it('should display error message when user clicks "Done" without selecting a pet', () => {
     fireEvent.click(screen.getByText('Done'));
     expect(screen.getByText('Please select a pet')).toBeInTheDocument();
   });
 
   it('should display error message when user clicks "Done" without entering a name for the pet', () => {
-    const onClose = jest.fn();
-    render(<PetSelectionPopup isOpen={true} onClose={onClose} />);
     fireEvent.click(screen.getByText('Cat'));
     fireEvent.click(screen.getByText('Done'));
     expect(screen.getByText('Please enter a name for your pet')).toBeInTheDocument();
+  });
+
+  it('should call onClose when user selects a pet and enters a name and clicks "Done"', () => {
+    const onClose = jest.fn();
+    render(<PetSelectionPopup isOpen={true} onClose={onClose} />);
+    fireEvent.click(screen.getByText('Cat'));
+    fireEvent.change(screen.getByPlaceholderText("Enter your pet's name"), {
+      target: { value: 'FakeName' },
+    });
+    fireEvent.click(screen.getByText('Done'));
+    expect(onClose).toHaveBeenCalled();
   });
 });
