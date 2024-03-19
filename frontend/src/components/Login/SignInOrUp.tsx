0a9e740df6ce71ea862899424ffd4bfee6abf652
shrey patel <patelsm0019@gmail.com>
2024-03-19 12:57:51 -0400

    integrated veda's version fo the login/signup page into mine

--- frontend/src/components/Login/SignInOrUp.tsx
+++ frontend/src/components/Login/SignInOrUp.tsx
@@ -1,8 +1,8 @@
 import { updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
-import { Button, FormControl, FormLabel, Heading, Input, Stack, useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, InputRightElement, Link, Stack, StylesProvider, useToast } from '@chakra-ui/react';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { set } from 'lodash';
+import { isError, set, values } from 'lodash';
 import { auth } from '../../firebase';
 import { FirebaseError } from 'firebase/app';
 
@@ -10,6 +10,7 @@ function SignInComponent(): JSX.Element {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSigningIn, setIsSigningIn] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const toast = useToast();
   const handleSignIn = async () => {
     setIsSigningIn(true);
@@ -44,33 +45,50 @@ function SignInComponent(): JSX.Element {
   };
   return (
     <>
-      <Stack>
-        <Heading as="h2" size="xl">Sign In</Heading>
-        <FormControl>
-          <FormLabel htmlFor='email'>Email Address</FormLabel>
-          <Input
-            autoFocus
-            name='email'
-            placeholder='test@example.com'
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <FormLabel htmlFor='password'>Password</FormLabel>
-          <Input
-            name='password'
-            type='password'
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-        </FormControl>
-        <Button 
-          data-testid='signInButton'
-          onClick={handleSignIn}
-          isLoading={isSigningIn}
-          isDisabled={isSigningIn}>
-          Sign in
-        </Button>
-      </Stack>
+      <Flex width='full' align='center' justifyContent='center'>
+        <Box p={8} maxWidth='500px' borderWidth={1} borderRadius={8} boxShadow='lg'>
+          <Box textAlign='center'>
+            <Heading>Login</Heading>
+          </Box>
+          <Box my={4} textAlign='left'>
+            <form>
+              <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type='email'
+                  placeholder='test@test.com'
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired mt={6}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='*******'
+                  onChange={event => setPassword(event.target.value)}
+                  value={password}
+                />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={event => setShowPassword(!showPassword)}>
+                    {showPassword ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </FormControl>
+              <Button mt={4}
+                width='full' 
+                type='submit' 
+                colorScheme='blue' 
+                isLoading={isSigningIn} 
+                isDisabled={isSigningIn}
+                onClick={handleSignIn}
+              >
+                Sign In
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
     </>
   );
 }
@@ -80,6 +98,7 @@ function SignUpComponent(): JSX.Element {
   const [password, setPassword] = useState('');
   const [userName, setUserName] = useState('');
   const [isSigningUp, setIsSigningUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const toast = useToast();
   const handleSignUp = async () => {
     setIsSigningUp(true);
@@ -119,39 +138,51 @@ function SignUpComponent(): JSX.Element {
   };
   return (
     <>
-      <Stack>
-        <Heading as="h2" size="xl">Sign Up</Heading>
-        <FormControl>
-          <FormLabel htmlFor='email'>Email Address</FormLabel>
-          <Input
-            autoFocus
-            name='email'
-            placeholder='test@example.com'
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <FormLabel htmlFor='password'>Password</FormLabel>
-          <Input
-            name='password'
-            type='password'
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-          <FormLabel htmlFor='username'>Username</FormLabel>
-          <Input
-            name='Username'
-            value={userName}
-            onChange={event => setUserName(event.target.value)}
-          />
-        </FormControl>
-        <Button 
-          data-testid='signInButton'
-          onClick={handleSignUp}
-          isLoading={isSigningUp}
-          isDisabled={isSigningUp}>
-          Sign up
-        </Button>
-      </Stack>
+      <Flex width='full' align='center' justifyContent='center'>
+        <Box p={8} maxWidth='500px' borderWidth={1} borderRadius={8} boxShadow='lg'>
+          <Box textAlign='center'>
+            <Heading>Login</Heading>
+          </Box>
+          <Box my={4} textAlign='left'>
+            <form>
+              <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type='email'
+                  placeholder='test@test.com'
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type='text'
+                  value={userName}
+                  onChange={event => setUserName(event.target.value)}
+                />
+              </FormControl>
+              <FormControl isRequired mt={6}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='*******'
+                  onChange={event => setPassword(event.target.value)}
+                  value={password}
+                />
+                <InputRightElement width='4.5rem'>
+                  <Button h='1.75rem' size='sm' onClick={event => setShowPassword(!showPassword)}>
+                    {showPassword ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </FormControl>
+              <Button mt={4} width='full' type='submit' colorScheme='blue' isDisabled={isSigningUp} isLoading={isSigningUp}>
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
     </>
   );
 }
@@ -162,14 +193,24 @@ function SignInOrUp(): JSX.Element {
   if (isSigningIn) {
     return (
       <>
-        No account? Sign up for one! <Button onClick={() => setIsSigningIn(false)}>Create New Account</Button>
+        <Flex width='full' align='center' justifyContent='center'>
+          <Stack align='center' justifyContent='center'>
+            <p>No account? Sign up for one!</p>
+            <Link color='blue' onClick={() => setIsSigningIn(false)}>Create New Account</Link>
+          </Stack>
+        </Flex>
         <SignInComponent />
       </>
     );
   } else {
     return (
       <>
-        Have an account already? Log in here: <Button onClick={() => setIsSigningIn(true)}>Sign in</Button>
+        <Flex width='full' align='center' justifyContent='center'>
+          <Stack align='center' justifyContent='center'>
+            <p>Have an account? Log in here:</p>
+            <Link color='blue' onClick={() => setIsSigningIn(true)}>Sign In</Link>
+          </Stack>
+        </Flex>
         <SignUpComponent />
       </>
     );
